ae803b79b70e33f5c7bd56b75f92955c8c0c180c
mpost_cp <SND\mpost_cp>
2011-07-12 13:58:46 -0700

    Initial checkin

---
+++ NuGetContentPackager/NuGetContentPackager/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace NuGetContentPackager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.namespaceTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(12, 97);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(574, 497);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = ".csproj |*.csproj|.vbproj|*.vbproj|.nugetpp|*.nugetpp";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open project file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save project";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Process and copy to NuGet directory";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // namespaceTextBox
+            // 
+            this.namespaceTextBox.Location = new System.Drawing.Point(109, 71);
+            this.namespaceTextBox.Name = "namespaceTextBox";
+            this.namespaceTextBox.Size = new System.Drawing.Size(237, 20);
+            this.namespaceTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Root namespace:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namespaceTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "NuGet Source Code Packager - Create PP files for NuGet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox namespaceTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NuGetContentPackager/NuGetContentPackager/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+
+namespace NuGetContentPackager
+{
+    public partial class Form1 : Form
+    {
+        TreeNode _contentNode = new TreeNode("Content");
+        DirectoryInfo _dirInfo;
+        string _contentFileNameTemplate = "{0}.nugetpp";
+        string _contentFileName = string.Empty;
+
+        XDocument _xdoc;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PopulateContentTreeView(TreeNode parent, DirectoryInfo directory)
+        {
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+            {
+                TreeNode node = new TreeNode(dir.Name);
+               
+                node.Tag = dir;
+                PopulateContentTreeView(node, dir);
+
+                if(node.Nodes.Count > 0)
+                    parent.Nodes.Add(node);
+
+            }
+
+            foreach (FileInfo f in directory.GetFiles())
+            {
+                TreeNode n = new TreeNode(f.Name);
+                
+                if (f.Name.ToLower().EndsWith(".cs")
+                    || f.Name.ToLower().EndsWith(".vb"))
+                {
+
+                    parent.Nodes.Add(n);
+
+
+                   
+
+                }
+                //(n.Tag as NugetPackageFile).PackageFile = n.FullPath;
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+           
+                foreach (var node in e.Node.Nodes)
+                {
+                    TreeNode tnode = node as TreeNode;
+
+                    if (tnode != null)
+                        tnode.Checked = e.Node.Checked;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog1.FileName);
+                _dirInfo = fileInfo.Directory;
+                folderBrowserDialog1.SelectedPath = _dirInfo.FullName;
+
+
+                string fileName = Path.GetFileNameWithoutExtension(fileInfo.FullName);
+
+                namespaceTextBox.Text = fileName;
+                _contentFileName = string.Format(_contentFileNameTemplate, fileName);
+
+                string filePath = Path.Combine(_dirInfo.FullName, _contentFileName);
+                if (new FileInfo(filePath).Exists)
+                {
+                    _xdoc = XDocument.Load(filePath);
+
+                    namespaceTextBox.Text = _xdoc.Element("nugetpp").Element("namespace").Value;
+                }
+
+                treeView1.Nodes.Add(_contentNode);
+
+                PopulateContentTreeView(_contentNode, _dirInfo);
+                _contentNode.Expand();
+
+                CheckNodes(_xdoc, _contentNode);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> paths = new List<string>();
+
+            FillPaths(paths, _contentNode);
+
+            XDocument xdoc = new XDocument();
+            XElement nugetpp = new XElement("nugetpp");
+
+            XElement files = new XElement("files");
+            foreach (string path in paths)
+            {
+                files.Add(new XElement("file", path));
+            }
+
+            xdoc.Add(nugetpp);
+
+            nugetpp.Add(files);
+            nugetpp.Add(new XElement("namespace", namespaceTextBox.Text));
+
+            xdoc.Save(Path.Combine(_dirInfo.FullName, _contentFileName));
+
+
+        }
+
+        private void CheckNodes(XDocument xdoc, TreeNode contentNode)
+        {
+            if (xdoc != null && xdoc.Element("nugetpp").Element("files").Elements("file").Where(x => x.Value == this.FullPath(contentNode, false)).Any())
+            {
+                contentNode.Checked = true;
+
+                if(contentNode.Parent != null)
+                    contentNode.Parent.Expand();
+
+                
+            }
+            else
+            {
+            }
+
+            foreach (var n in contentNode.Nodes)
+            {
+                TreeNode node = n as TreeNode;
+                CheckNodes(xdoc, node);
+            }
+        }
+
+        private void FillPaths(List<string> paths, TreeNode contentNode)
+        {
+            if (contentNode.Checked)
+            {
+                paths.Add(FullPath(contentNode, false));
+            }
+
+            foreach (var n in contentNode.Nodes)
+            {
+                TreeNode node = n as TreeNode;
+                FillPaths(paths, node);
+            }
+        }
+
+        private string FullPath(TreeNode node, bool includeParent)
+        {
+            if (node.Parent != null)
+            {
+                return FullPath(node.Parent, includeParent) + "\\" + node.Text;
+            }
+            else
+            {
+                if (includeParent)
+                    return "\\" + node.Text;
+                else
+                    return string.Empty;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                List<string> paths = new List<string>();
+
+                FillPaths(paths, _contentNode);
+
+                foreach (string filePath in paths)
+                {
+                    FileInfo fileInfo = new FileInfo(Path.Combine(_dirInfo.FullName, filePath.Substring(1)));
+
+                    if (fileInfo.Exists)
+                    {
+                        ProcessFile(fileInfo, filePath, folderBrowserDialog1.SelectedPath);
+                    }
+                }
+            }
+        }
+
+        private void ProcessFile(FileInfo fileInfo, string filePath, string targerPath)
+        {
+            string directory = new FileInfo(Path.Combine(targerPath, filePath.Substring(1) + ".pp")).Directory.FullName;
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            FileInfo copy = fileInfo.CopyTo(Path.Combine(targerPath, filePath.Substring(1) + ".pp"), true);
+
+            string orignal = File.ReadAllText(copy.FullName);
+
+            string modified = orignal.Replace(namespaceTextBox.Text, "$rootnamespace$");
+
+            File.WriteAllText(copy.FullName, modified);
+
+
+
+
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Parent != null)
+            {
+                e.Node.Parent.Expand();
+            }
+        }
+
+       
+    }
+}
